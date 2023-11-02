92cc433511c139059868ea16f2b22e59c5b16335
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2023-11-02 11:18:15 +0700

    Add another set of lesson files for data processing

--- data_processing_oop.py
+++ data_processing_oop.py
@@ -1,7 +1,4 @@
-import csv
-import os
-import copy
-
+import csv, os
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -18,7 +15,6 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
-
 class DB:
     def __init__(self):
         self.database = []
@@ -31,13 +27,13 @@ class DB:
             if table.table_name == table_name:
                 return table
         return None
-
-
+    
+import copy
 class Table:
     def __init__(self, table_name, table):
         self.table_name = table_name
         self.table = table
-
+    
     def join(self, other_table, common_key):
         joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
         for item1 in self.table:
@@ -48,20 +44,20 @@ class Table:
                     dict1.update(dict2)
                     joined_table.table.append(dict1)
         return joined_table
-
+    
     def filter(self, condition):
         filtered_table = Table(self.table_name + '_filtered', [])
         for item1 in self.table:
             if condition(item1):
                 filtered_table.table.append(item1)
         return filtered_table
-
+    
     def aggregate(self, function, aggregation_key):
         temps = []
         for item1 in self.table:
             temps.append(float(item1[aggregation_key]))
         return function(temps)
-
+    
     def select(self, attributes_list):
         temps = []
         for item1 in self.table:
@@ -74,7 +70,6 @@ class Table:
 
     def __str__(self):
         return self.table_name + ':' + str(self.table)
-
 
 table1 = Table('cities', cities)
 table2 = Table('countries', countries)
@@ -91,9 +86,9 @@ print(my_table1_selected)
 temps = []
 for item in my_table1_filtered.table:
     temps.append(float(item['temperature']))
-print(sum(temps) / len(temps))
+print(sum(temps)/len(temps))
 print("Using aggregation")
-print(my_table1_filtered.aggregate(lambda x: sum(x) / len(x), 'temperature'))
+print(my_table1_filtered.aggregate(lambda x: sum(x)/len(x), 'temperature'))
 
 print()
 my_table2 = my_DB.search('countries')
